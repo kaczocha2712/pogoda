bf1356c7648aaabc2d04c8178e1963db0dfcc5c0
PaulinaKaczocha <paulinka7311@gmail.com>
2024-11-19 12:19:04 +0100

    added search function using the API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,40 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+    
+
+
+cityElement.innerHTML = response.data.city;
+temperatureElement.innerHTML = Math.round(temperature);
+
+
+
+}
+
+function searchCity(city){
+let apiKey = "53f2071330t4ac47ab120653c94o340f";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+axios.get(apiUrl).then(refreshWeather);
+
+}
+
+
+
+
+
+
+
 function handleSearchSubmit(event){
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
-let cityElement = document.querySelector("#city");
-cityElement.innerHTML = searchInput.value;
+
+searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+
+searchCity("Mielec");
