7bd681e75b0992437b29c3c7029d8888266a7113
PaulinaKaczocha <paulinka7311@gmail.com>
2024-11-20 11:08:44 +0100

    injectet more weather date

--- src/index.js
+++ src/index.js
@@ -2,14 +2,35 @@ function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
     let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    let timeElement = document.querySelector("#time");
+    let date = new Date(response.data.time * 1000);
+
     
 
 
+timeElement.innerHTML = formatDate(date);    
 cityElement.innerHTML = response.data.city;
+windElement.innerHTML = `${response.data.wind.speed}km/h`;
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+descriptionElement.innerHTML = response.data.condition.description;
 temperatureElement.innerHTML = Math.round(temperature);
+}
 
+function formatDate(date) {
+  
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
+    let days =  ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = days[date.getDay()];
 
+    if (minutes < 10){
+        minutes = `0${minutes}`;
+    }
 
+     return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city){
@@ -35,6 +56,3 @@ searchCity(searchInput.value);
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
-
-
-searchCity("Mielec");
